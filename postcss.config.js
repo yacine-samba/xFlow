239e37965c30cd6b9c599bc28b0e52e5137a5aaf
Yacine Samba <ysambamezaache@gmail.com>
2025-04-07 10:49:25 +0200

    fix postcss-preset-env for vercel build

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-    plugins: {
-      'postcss-preset-env': {},
+  plugins: {
+    'postcss-preset-env': {
+      stage: 1,
+      features: {
+        'nesting-rules': true
+      }
     },
-  };
+    autoprefixer: {}
+  }
+}
